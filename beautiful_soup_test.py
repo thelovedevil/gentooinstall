995f4980979fd74fc0d5264cfe9198b2b4b5db90
adrian <adrian@kali.angel>
2024-05-14 12:22:20 -0400

    thirdtuesday working on url beautifulsoup

---
+++ beautiful_soup_test.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+import json
+import pandas as pd
+import httplib2
+from bs4 import BeautifulSoup, SoupStrainer
+
+def find_link():
+    http = httplib2.Http()
+    sources_list = []
+    status, response = http.request('https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-hardened-selinux-openrc/')
+    for link in BeautifulSoup(response, parse_only=SoupStrainer('a'), features="xml").find_all('a', href=True):
+        print(link['href'])
+        sources_list.append(link['href'])
+    df = pd.DataFrame(sources_list)
+    print(df)
+
+find_link()
+        
--- block_device_table.py
+++ block_device_table.py
@@ -6,25 +6,6 @@ import json
 import pandas as pd
 from cursesprint import print_curses
 
-
-class DictionaryDevice:
-    def __init__(self, dictionary_devices):
-        self.dictionary_devices = dictionary_devices.get("blockdevices",[])
-        for device in self.dictionary_devices:
-                for key, value in dictionary_devices.items():
-                        setattr(self, key, value)
-
-        def __iter__(self):
-            for device in self.dictionary_devices:
-                for value in self.dictionary_devices.values():
-                        yield value
-
-        def dictionarydeviceiter(self):
-            return iter(self.dictionary_devices.values())
-
-def dictionary_test_table(dictionary_command_line):
-    return DictionaryDevice(dictionary_command_line)
-
 def return_pandas():
     process = subprocess.run("lsblk --json -o NAME,SIZE,UUID,MOUNTPOINT,PATH,FSTYPE ".split(), capture_output=True, text=True)
     data = json.loads(process.stdout)
@@ -38,35 +19,12 @@ def return_pandas():
     print(df) 
     return df
 
-def return_blockdev_name():
-        process = subprocess.run("lsblk --json -o NAME,SIZE,UUID,MOUNTPOINT,PATH,FSTYPE ".split(), capture_output=True, text=True)
-        return json.loads(process.stdout)
-
 new_table = return_pandas()
-
-
-block_devices = return_blockdev_name()
-
-
-dict_table = dictionary_test_table(block_devices)
 
 def main(stdscr):
     x = 0
     stdscr = curses.initscr()
-    m = 0
-    def index():
-        index = 0
-        list = []
-        while index < len(dict_table.dictionary_devices):
-            index += 1
-            list.append(len(dict_table.dictionary_devices[index]))
-            list.sort()
-            for x in list:
-                if max(list) == len(dict_table.dictionary_devices[index].values()):
-                    maximum_of_list = max(list)
-                    return index
     
-    indexed = index()
 
     table = Table(stdscr, len(new_table), (len(new_table.columns)), 20, 100, 10, spacing=1, col_names=True)
 
