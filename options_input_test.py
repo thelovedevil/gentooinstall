a62e3321b032b2f641557a47ab2dea47e05d0693
adrian <adrian@kali.angel>
2024-06-05 23:59:34 -0400

    thirdtuesday

--- options_input_test.py
+++ options_input_test.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup, SoupStrainer
 from cursesprint import print_curses
 from testtest import sources_testcrypt
 from cryptsetup_table import crypt_options_digest, test_crypt_options
+import itertools
 
 stdscr = cur.initscr()
 
@@ -16,21 +17,26 @@ crypt_sources = test_crypt_options()
 def options_input():
     dictionary = {}
     print_curses(stdscr, "please enter the number of entries to enter n:")
-    n = int(input("enter number of options: "))
+    n = int(input("enter range of list to create: <should only need value (1) >"))
 
     for i in range(n):
-        print_curses(stdscr, "enter an option")
+        print_curses(stdscr, "enter options by scrolling selecting with enter then looping back through and selecting again")
         option = []
         option = crypt_options_digest(stdscr, crypt_sources)
-        command = []
-        print_curses(stdscr, "enter the value for that option by pressing r")
-        command = crypt_options_digest(stdscr, crypt_sources)
-        final = [j for i in zip(option, command) for j in i]
+        value = []
+        print_curses(stdscr,"enter the value for that option in the same manner by pressing r filling and selecting with enter")
+        value = crypt_options_digest(stdscr, crypt_sources)
+        final = [j for i in zip(option, value) for j in i]
         print(final)
+        return final
 
+command = []  
+command = options_input()
+print(command)
 
 
 def luks_process_prefab():              
-    luks_process = subprocess.run(['sudo', 'cryptsetup', variable_one[0], variable_two[0], variable_three[0], variable_four[0], variable_five[0], variable_six[0], variable_seven[0], variable_eight[0], 'luksFormat', '/dev/sda'])
+    luks_process = subprocess.run(['sudo', 'cryptsetup'] + command + ['luksFormat', '/dev/sda'])
 
-options_input()
+luks_process_prefab()
+
